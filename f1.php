d68953cf8f9e24a38cb7dd7cc2fd184f35c79aaf
khoiq6 <75017376+khoiq6@users.noreply.github.com>
2020-11-28 15:15:28 +0700

    khoi

---
+++ f1.php
@@ -0,0 +1,28 @@
+<?php
+@system('clear');
+echo "┌─┐ ─┐☮
+│▒│ /▒/
+│▒│/▒/
+│▒ /▒/─┬─┐
+│▒│▒|▒│▒│
+┌┴─┴─┐-┘─┘
+│▒┌──┘▒▒▒│
+└┐▒▒▒▒▒▒“” Yeah,Yeah!!!!!!!!
+";
+echo "➤ Chào mừng bạn đã đến với nền giáo dục php tiên tiến\n";
+sleep(1);
+echo "➤ Được phát triển bởi đội ngũ Termux Việt Nam\n";
+echo "➤ Mọi chi tiết liên hệ 113@canhsat.vn\n";
+sleep(1);
+echo "➤ Chúc các bạn 1 ngày vui vẻ\n";
+echo "✿✿✿✿✿✿✿✿✿✿✿✿✿✿✿✿✿✿✿✿✿✿\n";
+echo "☠ Bạn tên gì (ghi không dấu): ";
+$pk=trim(fgets(STDIN));
+echo "☠ Bạn ở đâu (tỉnh nha): ";
+$mk=trim(fgets(STDIN));
+echo "☠ Bạn thật hạnh phúc khi sinh ra ở đó!!!\n\n";
+while(true){
+  sleep(10);
+  echo "☠ Nhưng tao vẫn tìm ra mày thằng đầu cắt moi\n";
+}
+?>
